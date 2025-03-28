4105aea2c1c266400147d714f8a64da417a23831
Ahmed Khan <ahmed.khan0470@gmail.com>
2025-03-28 16:09:49 +0000

    added testing

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from --Ahmed Khan-- in 3308'
+
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect(postgresql://ahmed_render_db_user:UGqZ1UOcGpoRVW10SSVWHeUqhHNlWVEf@dpg-cvjchvripnbc73e1cqkg-a/ahmed_render_db)
+    conn.close()
+    return "Database connection successful"
